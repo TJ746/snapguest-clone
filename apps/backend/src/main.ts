88813bbced0e39436ec7d9d10e736ab7144689b2
Tinon Jacob <tinon.jacob@qualicasa.ch>
2025-09-19 01:40:50 +0200

    Fix: Set CORS to allow all origins temporarily

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,7 @@ declare const process: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://frontend-ee0ati0my-tjs-projects-e59fb6d9.vercel.app'],
+    origin: true, // Allow all origins for now
     credentials: true,
   });
   const port = process.env.PORT || 3001;
